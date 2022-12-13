be0b42448019ab2334023d249f0f03519fe03cdb
Craig M. Bender <craigbender@hpz600.craigbender.me>
2022-12-13 05:42:56 -0800

    post docker hub sync

--- mtr-docker/scripts/def-ip.sh
+++ mtr-docker/scripts/def-ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export DEF_IP=$(ip a show dev $(ip -o -4 r show default|grep -m1 -oP "(?<=dev )[^ ]+")|grep -m1 -oP "(?<=inet )[^/]+")
+export DEF_IP=$(bash -c 'ip a show dev $(ip -o -4 r show default|grep -m1 -oP "(?<=dev )[^ ]+")|grep -m1 -oP "(?<=inet )[^/]+"')
 echo $DEF_IP
